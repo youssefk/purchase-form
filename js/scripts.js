6d4d9c01e171a9b0cf7e86cf66af42cc6af03635
Youssef <youssefkhalifa@live.com>
2015-10-05 20:47:11 -0700

    Tweak receipt styling, add second address line.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var firstnameInput = $("input#firstname").val();
     var lastnameInput = $("input#lastname").val();
     var addressInput = $("input#address").val();
+    var address2Input = $("input#address2").val();
     var cityInput = $("input#city").val();
     var stateInput = $("input#state").val();
     var zipInput = $("input#zip").val();
@@ -13,6 +14,7 @@ $(document).ready(function() {
     $(".firstname").text(firstnameInput);
     $(".lastname").text(lastnameInput);
     $(".address").text(addressInput);
+    $(".address2").text(address2Input);
     $(".city").text(cityInput);
     $(".state").text(stateInput);
     $(".zip").text(zipInput);
