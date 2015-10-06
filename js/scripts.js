9738668e2959cb7c70175e10d7837bc44fedd59d
Youssef <youssefkhalifa@live.com>
2015-10-05 20:39:35 -0700

    Add javascript to make forms submittable and print receipt.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var firstnameInput = $("input#firstname").val();
+    var lastnameInput = $("input#lastname").val();
+    var addressInput = $("input#address").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipInput = $("input#zip").val();
+    var emailInput = $("input#email").val();
+    var phoneInput = $("input#phone").val();
+
+
+    $(".firstname").text(firstnameInput);
+    $(".lastname").text(lastnameInput);
+    $(".address").text(addressInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
+    $(".email").text(emailInput);
+    $(".phone").text(phoneInput);
+
+    $("#receipt").show();
+
+    event.preventDefault();
+
+  });
+});
